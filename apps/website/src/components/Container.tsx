913b3532955ea8b17ff6cdc27b88c746342de1c3
Ian Hunter <ian@ianh.xyz>
2024-07-30 08:03:51 -0500

    no longer hosting website here

--- apps/website/src/components/Container.tsx
+++
@@ -1,7 +0,0 @@
-export const Container = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="w-full mx-auto md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl px-4 sm:py-16 lg:px-6">
-      {children}
-    </div>
-  );
-};
--- apps/website/src/components/Logo.tsx
+++
@@ -1,3 +0,0 @@
-export const Logo = () => (
-  <span className="font-serif font-bold">🚀 StartupKit</span>
-);
