45ae840e57592508788fc975c97dd1c6df902189
Ian Hunter <ian@ianh.xyz>
2024-08-16 09:29:20 -0500

    cleanup

--- template/src/app/(authenticated)/dash/page.tsx
+++ template/src/app/(authenticated)/dash/page.tsx
@@ -3,28 +3,16 @@ import { SignInButton } from "@/components/app/signin-button";
 import { getUser } from "@startupkit/auth/server";
 
 export default async function HomePage() {
-  const { isAuthenticated, user } = await getUser();
+  const { user } = await getUser();
 
   return (
     <div className="flex flex-col items-center justify-center">
-      {isAuthenticated ? (
-        <>
-          <h2 className="text-2xl font-bold">
-            Welcome back{user?.firstName && `, ${user?.firstName}`}
-          </h2>
-          <p>You are now authenticated into the application</p>
-          <div className="flex space-x-2">
-            <NextLink href="/account">View account</NextLink>
-            <SignInButton />
-          </div>
-        </>
-      ) : (
-        <>
-          <h2 className="text-2xl font-bold">Welcome to Startupkit</h2>
-          <p>Sign in to view your account details</p>
-          <SignInButton />
-        </>
-      )}
+      <>
+        <h2 className="text-2xl font-bold">
+          Welcome back{user?.firstName && `, ${user?.firstName}`}
+        </h2>
+        <p>You are now authenticated into the application</p>
+      </>
     </div>
   );
 }
