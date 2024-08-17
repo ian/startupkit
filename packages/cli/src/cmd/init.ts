b73c4402bd5f0f64dfb4edc3c08d4555f7964ab9
Ian H <ian@users.noreply.github.com>
2024-08-17 13:23:38 -0500

    CLI package replace (#7)
    * fixing install
    
    * pnpm publish to remove workspace deps
    
    * bump
    
    * remove npm, update package, fix init, update client providers to be ssr safe, cleanup tsconfig

--- packages/cli/src/cmd/init.ts
+++ packages/cli/src/cmd/init.ts
@@ -27,6 +27,8 @@ const questions: any[] = [
 export async function init() {
   opener();
 
+  const stdio = process.env.DEBUG ? "inherit" : "ignore";
+
   const answers: Answers = await inquirer.prompt(questions);
   const baseDir = process.env.SK_DIR ?? process.cwd();
   const destPath = baseDir + "/" + answers.name;
@@ -37,16 +39,16 @@ export async function init() {
     const example =
       process.env.EXAMPLE ?? "https://github.com/01-studio/startupkit";
     const examplePath = "template";
-    const installCmd = `npx create-next-app@latest ${destPath} --use-pnpm --example ${example} --example-path "${examplePath}"`;
+    const installCmd = `npx create-next-app ${destPath} --use-pnpm --skip-install --example ${example} --example-path "${examplePath}"`;
 
     await exec(installCmd, {
-      stdio: "inherit",
+      stdio,
     });
 
     await writeFile(
       `${destPath}/.env.local`,
       `
-DATABASE_URL="postgresql://localhost:5432/${answers.name}?schema=public"
+DATABASE_URL="postgresql://postgres@localhost:5432/${answers.name}?schema=public"
 
 # Auth
 AUTH_SECRET=FAKE1234567890123456789012345678901234567890
@@ -65,16 +67,41 @@ STRIPE_SECRET_KEY=
 STRIPE_WEBHOOK_SECRET=
 `,
     );
+
+    const pkg = JSON.parse(await readFile(`${destPath}/package.json`));
+
+    pkg.name = answers.name;
+
+    pkg.dependencies = Object.fromEntries(
+      Object.entries(pkg.dependencies).map(([key, value]) =>
+        value === "workspace:*" ? [key, process.env.VERSION] : [key, value],
+      ),
+    );
+
+    pkg.devDependencies = Object.fromEntries(
+      Object.entries(pkg.devDependencies).map(([key, value]) =>
+        value === "workspace:*" ? [key, process.env.VERSION] : [key, value],
+      ),
+    );
+
+    await writeFile(`${destPath}/package.json`, JSON.stringify(pkg, null, 2));
   });
 
-  await spinner("Adding StartupKit", async () => {
-    const packages = ["analytics", "auth", "cms", "payments"];
-    const installCmd = `pnpm add ${packages.map((p) => `@startupkit/${p}@${version}`).join(" ")}`;
-    await exec(installCmd, {
-      stdio: "inherit",
+  await spinner("Installing StartupKit", async () => {
+    await exec("pnpm install", {
+      stdio,
       cwd: destPath,
     });
   });
+
+  // await spinner("Adding StartupKit", async () => {
+  //   const packages = ["analytics", "auth", "cms", "payments"];
+  //   const installCmd = `pnpm add ${packages.map((p) => `@startupkit/${p}@${version}`).join(" ")}`;
+  //   await exec(installCmd, {
+  //     stdio,
+  //     cwd: destPath,
+  //   });
+  // });
 }
 
 function opener() {
