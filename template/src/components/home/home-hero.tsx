2908e27e2f58405d0096b96a60e952762a6f9aa0
Ian Hunter <ian@ianh.xyz>
2024-09-01 11:33:47 -0500

    basic layout

--- template/src/components/home/home-hero.tsx
+++ template/src/components/home/home-hero.tsx
@@ -1,39 +1,43 @@
 import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
 import { Logo } from "../logo";
 
 export function Hero() {
 	return (
-		<div className="">
-			<div className="relative isolate pt-14">
-				<div className="">
-					<div className="px-6 mx-auto max-w-7xl lg:px-8">
-						<div className="max-w-3xl mx-auto text-center">
-							<h1 className="text-4xl font-bold tracking-tight text-gray-900">
-								Welcome to <Logo className="inline h-16 ml-4" />
-							</h1>
-							<p className="mt-6 text-lg leading-8 text-gray-600">
-								Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-								lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-								fugiat aliqua.
-							</p>
-							<div className="flex items-center justify-center mt-10 gap-x-6">
-								<Link
-									href="/api/auth/login"
-									className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-								>
-									Get started
-								</Link>
-								<a
-									href="https://startupkit.com"
-									className="text-sm font-semibold leading-6 text-gray-900"
-								>
-									Learn more <span aria-hidden="true">→</span>
-								</a>
+		<div className="flex flex-col min-h-screen">
+			<main className="flex-1">
+				<section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
+					<div className="max-w-xl mx-auto px-4 md:px-6">
+						<div className="flex flex-col items-center space-y-4 text-center">
+							<div className="space-y-2">
+								<h1 className="text-4xl font-bold tracking-tight text-gray-900">
+									Welcome to <Logo className="inline h-16 ml-4" />
+								</h1>
+								<p className="mx-auto max-w-[700px] md:text-xl text-gray-500">
+									StartupKit is Next.js but with all the addons a startup will
+									need to launch. Out of the box you get auth, analytics, cms,
+									payments, and more.
+								</p>
+							</div>
+							<div className="w-full space-y-2">
+								<div className="mx-auto grid grid-cols-2 gap-2">
+									<Link href="https://docs.startupkit.com">
+										<Button className="w-full" variant="outline">
+											Read the Docs
+										</Button>
+									</Link>
+									<Link href="https://docs.startupkit.com/v0">
+										<Button className="w-full" variant="outline">
+											Using v0 with StartupKit
+										</Button>
+									</Link>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-			</div>
+				</section>
+			</main>
 		</div>
 	);
 }
