5b100e31f0627f6e4562245cbcb1f1cd6f4cb41a
Ian Hunter <ian@ianh.xyz>
2024-09-05 19:20:28 -0500

    add free stock photo sites

--- template/next.config.mjs
+++ template/next.config.mjs
@@ -5,14 +5,15 @@ import { withPayments } from "@startupkit/payments/config";
 
 /** @type {import('next').NextConfig} */
 const baseConfig = {
-	env: {
-		STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY,
-	},
+  env: {},
+  images: {
+    domains: ["pexels.com", "unsplash.com"],
+  },
 };
 
 /** Combine all the plugins into a single configuration */
 const combine = (...plugins) => {
-	return plugins.reduce((acc, plugin) => plugin(acc), baseConfig);
+  return plugins.reduce((acc, plugin) => plugin(acc), baseConfig);
 };
 
 export default combine(withAnalytics(), withAuth(), withCMS(), withPayments());
