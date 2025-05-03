1886a8232abe57ad0e2848ed23809f774c4977e8
Ian Hunter <ian@ianh.xyz>
2025-05-03 17:50:18 -0500

    remove old template

--- template/src/middleware.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export function middleware() {
-  return NextResponse.next();
-}
-
-// Match against the account page
-export const config = { matcher: ["/account/:path*"] };
