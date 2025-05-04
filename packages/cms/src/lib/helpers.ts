f243af8c2cee3bccfc862da359fbcf91e28a122e
Ian Hunter <ian@ianh.xyz>
2025-05-03 22:13:22 -0500

    remove payments and cms

--- packages/cms/src/lib/helpers.ts
+++
@@ -1,8 +0,0 @@
-import path from "path";
-
-export const extractRoute = (filePath: string): string | null => {
-  const dir = path.dirname(filePath); // Get the directory name
-  const fileName = path.basename(filePath, path.extname(filePath)); // Get the file name without extension
-  const match = dir.match(/\/pages(\/.*)/); // Match the path segment
-  return match ? `${match[1]}/${fileName}` : null; // Return the matched route with the file name
-};
--- packages/payments/src/server.ts
+++
@@ -1,4 +0,0 @@
-export * from "./server/checkoutWithStripe";
-export * from "./server/getPortalLink";
-export * from "./server/getProducts";
-export * from "./server/getSubscription";
