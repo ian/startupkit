a34c36befd37ca738ad33995d0188a2acf74fdc1
Ian Hunter <ian@ianh.xyz>
2025-10-13 07:07:12 +1100

    export more

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -2,7 +2,29 @@ export * from "./components"
 export { createAuth } from "./lib/auth"
 export * from "./types"
 
-// Re-export better-auth utilities for full flexibility
-export * from "better-auth/client/plugins"
+export {
+    adminClient,
+    anonymousClient,
+    apiKeyClient,
+    clientSideHasPermission,
+    customSessionClient,
+    emailOTPClient,
+    genericOAuthClient,
+    getPasskeyActions,
+    inferAdditionalFields,
+    inferOrgAdditionalFields, InferServerPlugin, jwtClient,
+    lastLoginMethodClient,
+    magicLinkClient,
+    multiSessionClient,
+    oidcClient,
+    oneTapClient,
+    oneTimeTokenClient,
+    organizationClient,
+    passkeyClient,
+    phoneNumberClient,
+    siweClient,
+    ssoClient,
+    usernameClient
+} from "better-auth/client/plugins"
 export { createAuthClient } from "better-auth/react"
 
