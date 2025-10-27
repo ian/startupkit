6b9146d87e31851b2a06a66a314d5cd7dc237f81
Ian Hunter <ian@ianh.xyz>
2025-10-27 12:57:47 +1100

    fix typecheck

--- packages/cli/src/cmd/init.ts
+++ packages/cli/src/cmd/init.ts
@@ -1,7 +1,8 @@
-import path from "node:path"
 import degit from "degit"
 import inquirer from "inquirer"
+import path from "node:path"
 import { replaceInFile } from "replace-in-file"
+import { spinner } from "../lib/spinner"
 import { exec } from "../lib/system"
 
 type Answers = {
