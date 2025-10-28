0ef15a8648d89bdecc55a5bff54b589629ea9ab8
ian <ian@users.noreply.github.com>
2025-10-29 05:53:42 +1100

    Remove packages/utils (#32)
    * remove utils we dont need this anymore:
    
    * more
    
    * remove this junk

--- templates/repo/packages/auth/src/lib/auth.ts
+++ templates/repo/packages/auth/src/lib/auth.ts
@@ -57,7 +57,9 @@ export const auth = betterAuth({
 			if (newSession) {
 				await db
 					.update(users)
-					.set({ lastSeenAt: new Date() })
+					.set({
+						lastSeenAt: new Date()
+					})
 					.where(eq(users.id, newSession.user.id))
 
 				const [user] = await db
