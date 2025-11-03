bd2406c9dd2e99b959fc32b0d299e8f8bdbe4188
Ian Hunter <ian@ianh.xyz>
2025-11-02 18:06:33 -0600

    fix

--- apps/home/src/components/StartupKitProvider.tsx
+++ apps/home/src/components/StartupKitProvider.tsx
@@ -1,29 +1,29 @@
-'use client';
+"use client"
 
 import {
-  AnalyticsProvider,
-  GoogleAnalytics,
-  OpenPanelPlugin,
-} from '@startupkit/analytics';
-import type { ReactNode } from 'react';
+	AnalyticsProvider,
+	GoogleAnalytics,
+	OpenPanelPlugin
+} from "@startupkit/analytics"
+import type { ReactNode } from "react"
 
 const plugins = [
-  GoogleAnalytics({
-    measurementId: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID as string,
-  }),
-  OpenPanelPlugin({
-    clientId: process.env.NEXT_PUBLIC_OPENPANEL_CLIENT_ID as string,
-  }),
-];
+	GoogleAnalytics({
+		measurementId: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID as string
+	}),
+	OpenPanelPlugin({
+		clientId: process.env.NEXT_PUBLIC_OPENPANEL_CLIENT_ID as string
+	})
+]
 
 export function StartupKitProvider({
-  children,
+	children
 }: Readonly<{
-  children: ReactNode;
+	children: ReactNode
 }>) {
-  return (
-    <AnalyticsProvider flags={{}} plugins={plugins}>
-      {children}
-    </AnalyticsProvider>
-  );
+	return (
+		<AnalyticsProvider flags={{}} plugins={plugins}>
+			{children}
+		</AnalyticsProvider>
+	)
 }
