240a5f85b1ab6c488d31674a22523abc384902e5
Ian Hunter <ian@ianh.xyz>
2025-11-02 18:41:49 -0600

    adding ahrefs

--- apps/home/src/components/StartupKitProvider.tsx
+++ apps/home/src/components/StartupKitProvider.tsx
@@ -1,29 +1,33 @@
-"use client"
+'use client';
 
 import {
-	AnalyticsProvider,
-	GoogleAnalytics,
-	OpenPanelPlugin
-} from "@startupkit/analytics"
-import type { ReactNode } from "react"
+  Ahrefs,
+  AnalyticsProvider,
+  GoogleAnalytics,
+  OpenPanelPlugin,
+} from '@startupkit/analytics';
+import type { ReactNode } from 'react';
 
 const plugins = [
-	GoogleAnalytics({
-		measurementId: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID as string
-	}),
-	OpenPanelPlugin({
-		clientId: process.env.NEXT_PUBLIC_OPENPANEL_CLIENT_ID as string
-	})
-]
+  Ahrefs({
+    key: process.env.NEXT_PUBLIC_AHREFS_API_KEY as string,
+  }),
+  GoogleAnalytics({
+    measurementId: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID as string,
+  }),
+  OpenPanelPlugin({
+    clientId: process.env.NEXT_PUBLIC_OPENPANEL_CLIENT_ID as string,
+  }),
+];
 
 export function StartupKitProvider({
-	children
+  children,
 }: Readonly<{
-	children: ReactNode
+  children: ReactNode;
 }>) {
-	return (
-		<AnalyticsProvider flags={{}} plugins={plugins}>
-			{children}
-		</AnalyticsProvider>
-	)
+  return (
+    <AnalyticsProvider flags={{}} plugins={plugins}>
+      {children}
+    </AnalyticsProvider>
+  );
 }
