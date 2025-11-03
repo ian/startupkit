eead9e3df20c9c5eb11e6d79ff2f1dc9c3dfe063
Ian Hunter <ian@ianh.xyz>
2025-11-02 20:35:36 -0600

    fix site builds

--- apps/home/next.config.mjs
+++ apps/home/next.config.mjs
@@ -1,23 +1,21 @@
-import { startupkitCMS } from "@startupkit/cms/plugin"
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	turbopack: {},
-	transpilePackages: [
-		"@startupkit/analytics",
-		"@startupkit/auth",
-		"@startupkit/utils"
-	],
-	rewrites: () => [
-		{
-			source: "/docs",
-			destination: "https://startupkit.mintlify.dev/docs"
-		},
-		{
-			source: "/docs/:match*",
-			destination: "https://startupkit.mintlify.dev/docs/:match*"
-		}
-	]
-}
+  turbopack: {},
+  transpilePackages: [
+    '@startupkit/analytics',
+    '@startupkit/auth',
+    '@startupkit/utils',
+  ],
+  //   rewrites: () => [
+  //     {
+  //       source: '/docs',
+  //       destination: 'https://startupkit.mintlify.dev/docs',
+  //     },
+  //     {
+  //       source: '/docs/:match*',
+  //       destination: 'https://startupkit.mintlify.dev/docs/:match*',
+  //     },
+  //   ],
+};
 
-export default startupkitCMS({})(nextConfig)
+export default nextConfig;
