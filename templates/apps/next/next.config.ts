d50124b85294bb226dff2af902d085fb040c2742
Ian Hunter <ian@ianh.xyz>
2025-11-06 18:30:42 -0600

    logspam gone finally

--- templates/apps/next/next.config.ts
+++ templates/apps/next/next.config.ts
@@ -1,7 +1,8 @@
+import { withMonorepoConfig } from "@config/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	transpilePackages: ["@repo/analytics", "@repo/auth", "@repo/ui", "@repo/utils"]
 };
 
-export default nextConfig;
+export default withMonorepoConfig(nextConfig);
