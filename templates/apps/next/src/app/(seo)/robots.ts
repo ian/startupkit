59680b3823b1b4159ef06800884a90d308c8fee8
Ian Hunter <ian@ianh.xyz>
2025-11-09 19:19:43 -0600

    fix getUrl

--- templates/apps/next/src/app/(seo)/robots.ts
+++ templates/apps/next/src/app/(seo)/robots.ts
@@ -12,6 +12,6 @@ export default function robots(): MetadataRoute.Robots {
 				disallow: ["/api/", "/dashboard/", "/auth/"]
 			}
 		],
-		sitemap: `${baseUrl}/sitemap.xml`
+		sitemap: getUrl("sitemap.xml")
 	}
 }
